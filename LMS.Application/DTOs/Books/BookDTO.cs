66f190a8476b5bdc98886ea81e4d6cfdee581771
lorik aliu <loriku123321@gmail.com>
2025-12-05 00:22:06 +0100

    added DTOs for Book entity

---
+++ LMS.Application/DTOs/Books/BookDTO.cs
@@ -0,0 +1,28 @@
+﻿using LMS.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.DTOs.Books
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public ReadingStatus ReadingStatus { get; set; }
+       
+        public int? PublicationYear { get; set; }
+       
+        public string? CoverImageUrl { get; set; }
+        public int? Rating { get; set; }
+       
+        public string UserId { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ LMS.Application/DTOs/Books/CreateBookDTO.cs
@@ -0,0 +1,25 @@
+﻿using LMS.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.DTOs.Books
+{
+    public class CreateBookDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+        public ReadingStatus ReadingStatus { get; set; } = ReadingStatus.NotStarted;
+
+        public int? PublicationYear { get; set; }
+
+        public string? CoverImageUrl { get; set; }
+
+        public int? Rating { get; set; }
+    }
+}
