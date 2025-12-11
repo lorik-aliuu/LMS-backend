84faa895731c372536a9363680dbd16f4725d6c5
lorik aliu <loriku123321@gmail.com>
2025-12-11 11:59:49 +0100

    added contracts that  i need to connect with open ai api key and implement the query logic

---
+++ LMS.Application/ServiceInterfaces/IAiQueryService.cs
@@ -0,0 +1,14 @@
+﻿using LMS.Application.DTOs.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.ServiceInterfaces
+{
+    public interface IAiQueryService
+    {
+        Task<AiQueryResponseDTO>ProcessQueryAsync(string query, string userId, bool isAdmin);
+    }
+}
---
+++ LMS.Application/ServiceInterfaces/IOpenAiService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Application.ServiceInterfaces
+{
+    public interface IOpenAiService
+    {
+        Task<string> AnalyzeQueryAsync(string userQuery, string context);
+        Task<string> GenerateAnswerAsync(string query, string data);
+    }
+}
